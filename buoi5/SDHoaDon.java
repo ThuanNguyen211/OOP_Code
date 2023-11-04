afde32d0c90d964007ee7e408d36a1fba694a954
ThuanNguyen211 <nmthuan211@gmail.com>
2023-11-04 23:48:45 +0700

    project_oop

--- buoi5/SDHoaDon.java
+++ buoi5/SDHoaDon.java
@@ -1,15 +1,15 @@
 package buoi5;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 public class SDHoaDon {
-	//private static Scanner sc;
+	private static Scanner sc;
 	public static void main(String[] args) {
 		    
 		HoaDon h1 = new HoaDon();
 		h1.nhap();
 		h1.in();
 		
-		/*
+		
 		HoaDon h2 = new HoaDon(h1);
 		h2.nhap();
 		h2.in();
@@ -26,6 +26,6 @@ public class SDHoaDon {
 		}
 		for(int i=0; i<n; i++) {
 			dsHD[i].in();
-		}*/
+		}
 	}
 }
